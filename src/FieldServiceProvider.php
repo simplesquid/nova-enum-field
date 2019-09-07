6968b3f14e4b0290a004a4235cb643a00f9ecd23
mdpoulter <github@poulter.co.za>
2019-09-07 22:19:51 +0200

    Initial commit.

---
+++ src/FieldServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright (c) 2019 Matthew Poulter. All rights reserved.
+ */
+
+namespace SimpleSquid\Nova\Fields\Enum;
+
+use Illuminate\Support\ServiceProvider;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
