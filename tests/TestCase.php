64d52a9861a28e3077f66d8755ca9ed66cb9b20b
mdpoulter <github@matthewpoulter.co.za>
2021-01-25 16:44:15 +0200

    Update minimum dependency.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,22 +5,14 @@ namespace SimpleSquid\Nova\Fields\Enum\Tests;
 use Illuminate\Database\Schema\Blueprint;
 use Laravel\Nova\NovaServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use SimpleSquid\Nova\Fields\Enum\EnumFieldServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
     protected function getPackageProviders($app)
     {
         return [
-            EnumFieldServiceProvider::class,
+            NovaServiceProvider::class,
         ];
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        app()->register(NovaServiceProvider::class);
     }
 
     protected function setUpDatabase($app, $type = 'integer')
