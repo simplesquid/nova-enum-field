9fb6a0f83d0fa9201d6a7f174e3de25bc9fc9f2a
mdpoulter <github@matthewpoulter.co.za>
2021-01-25 16:50:24 +0200

    Add tests for lack of casts.

---
+++ tests/Fields/NoCastsFieldTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SimpleSquid\Nova\Fields\Enum\Tests\Fields;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+use SimpleSquid\Nova\Fields\Enum\Enum;
+use SimpleSquid\Nova\Fields\Enum\Tests\Examples\NoCastsModel;
+use SimpleSquid\Nova\Fields\Enum\Tests\Examples\StringEnum;
+use SimpleSquid\Nova\Fields\Enum\Tests\TestCase;
+
+class NoCastsFieldTest extends TestCase
+{
+    private $field;
+
+    private $model;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase($this->app, 'string');
+
+        $this->field = Enum::make('Enum')->attach(StringEnum::class);
+
+        $this->model = NoCastsModel::create(['enum' => StringEnum::Moderator]);
+    }
+
+    /** @test */
+    public function it_resolves_enum_value()
+    {
+        $this->field->resolve($this->model);
+
+        $this->assertSame(StringEnum::Moderator, $this->field->value);
+    }
+
+    /** @test */
+    public function it_displays_enum_description()
+    {
+        $this->field->resolveForDisplay($this->model);
+
+        $this->assertSame(StringEnum::Moderator, $this->field->value);
+    }
+
+    /** @test */
+    public function it_fills_database_with_enum_value()
+    {
+        $request = new NovaRequest();
+        $request->query->add(['enum' => StringEnum::Subscriber]);
+
+        $this->field->fill($request, $this->model);
+
+        $this->assertDatabaseHas('example_models', ['enum' => StringEnum::Moderator]);
+
+        $this->model->save();
+
+        $this->assertDatabaseHas('example_models', ['enum' => StringEnum::Subscriber]);
+
+        $this->assertDatabaseMissing('example_models', ['enum' => StringEnum::Moderator]);
+    }
+}
