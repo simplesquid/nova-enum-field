25d6104460495daaf12d5c1dbfe6ccdea7050239
Matthew Poulter <poulter@vcsw.nl>
2022-02-21 23:11:07 +0100

    wip

--- tests/Examples/FlaggedModel.php
+++ tests/Examples/FlaggedModel.php
@@ -2,13 +2,10 @@
 
 namespace SimpleSquid\Nova\Fields\Enum\Tests\Examples;
 
-use BenSampo\Enum\Traits\CastsEnums;
 use Illuminate\Database\Eloquent\Model;
 
 class FlaggedModel extends Model
 {
-    use CastsEnums;
-
     public $table = 'example_models';
 
     protected $guarded = [];
